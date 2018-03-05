fd595f7600695864373c21d96158785ff7f1e123
Orace <pierre.lando@gmail.com>
2018-03-05 11:03:18 +0100

    Initial commit.

---
+++ Dsubstitator/Data.cs
@@ -0,0 +1,66 @@
+﻿// 
+// This file is a part of the Dsubstitator project.
+// https://github.com/Orace
+// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dsubstitator
+{
+    public class Data
+    {
+        private readonly Dictionary<long, List<string>> _dictionary = new Dictionary<long, List<string>>();
+
+        public IReadOnlyList<string> GetMatch(string word)
+        {
+            word = word.ToLower();
+            var key = GetKey(word);
+            var matchs = _dictionary[key];
+            var code = GetCode(word).ToList();
+            var result = matchs.Where(m => GetCode(m).SequenceEqual(code)).ToList();
+
+            if (matchs.Count != result.Count)
+            {
+                Console.WriteLine($"{matchs.Count - result.Count} colisions detected");
+            }
+
+            return result;
+        }
+
+        public void Register(string word)
+        {
+            word = word.ToLower();
+            var key = GetKey(word);
+            if (!_dictionary.TryGetValue(key, out var list))
+            {
+                list = new List<string>();
+                _dictionary.Add(key, list);
+            }
+            list.Add(word);
+        }
+
+        private static long GetKey(string word)
+        {
+            unchecked
+            {
+                return GetCode(word).Aggregate(0, (current, i) => current * 397 ^ i);
+            }
+        }
+
+        private static IEnumerable<int> GetCode(string word)
+        {
+            var knownChar = new Dictionary<char, int>();
+            foreach (var c in word)
+            {
+                if (!knownChar.TryGetValue(c, out var index))
+                {
+                    index = knownChar.Count;
+                    knownChar.Add(c, index);
+                }
+                yield return index;
+            }
+        }
+    }
+}
---
+++ Dsubstitator/Program.cs
@@ -0,0 +1,57 @@
+﻿// 
+// This file is a part of the Dsubstitator project.
+// https://github.com/Orace
+// 
+
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Dsubstitator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Please provide a file path for words database");
+                Console.ReadLine();
+            }
+
+            var data = new Data();
+            using (var file = File.OpenRead(args[0]))
+            {
+                using (var sr = new StreamReader(file))
+                {
+                    var count = 0;
+                    while (!sr.EndOfStream)
+                    {
+                        var l = sr.ReadLine();
+                        if (l == null) continue;
+                        if (l.StartsWith("#")) continue;
+                        var w = l.Split(' ').First();
+                        data.Register(w);
+                        count++;
+                    }
+
+                    Console.WriteLine($"{count} words registered.");
+                }
+            }
+
+            for (;;)
+            {
+                Console.Write("Enter a word: ");
+                var w = Console.ReadLine();
+                var matches = data.GetMatch(w);
+                Console.WriteLine($"{matches.Count} matchs for: '{w}'");
+                foreach (var match in matches)
+                {
+                    Console.WriteLine(match);
+                }
+                Console.WriteLine("---");
+                Console.WriteLine();
+            }
+        }
+    }
+}
